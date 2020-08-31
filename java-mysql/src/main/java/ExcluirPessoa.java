6eb6a6df023f6992eb8a13c51a2f890bee292cce
marcelo.bn <marcelobittencourtx@gmail.com>
2020-08-30 22:27:43 -0300

    excluindo registros aula 312

---
+++ java-mysql/src/main/java/ExcluirPessoa.java
@@ -0,0 +1,33 @@
+import db.ConnectionFactory;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class ExcluirPessoa {
+    public static void main(String[] args) {
+        Connection cnx = ConnectionFactory.getDBConnection();
+        String sql = "DELETE FROM pessoa WHERE codigo = ?";
+        int id = 4;
+
+        PreparedStatement stmt = null;
+
+        try {
+            stmt = cnx.prepareStatement(sql);
+            stmt.setInt(1,id);
+
+            int contador = stmt.executeUpdate();
+            if(contador > 0) {
+                System.out.println("Pessoa excluída com sucesso!");
+                System.out.println("Linhas afetadas: "+contador);
+            } else {
+                System.out.println("Nada ocorreu...");
+            }
+            stmt.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
