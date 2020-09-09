3db23b9b803e994d1fa3f7f92b31ea1c43aab12b
marcelo.bn <marcelobittencourtx@gmail.com>
2020-09-09 09:28:32 -0300

    Validando produtos aula 427

--- exercicios-sb/src/main/java/com/example/exerciciossb/model/entities/Produto.java
+++ exercicios-sb/src/main/java/com/example/exerciciossb/model/entities/Produto.java
@@ -4,6 +4,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
 
 @Entity
 public class Produto {
@@ -11,18 +14,27 @@ public class Produto {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
+	
+	@NotBlank
 	private String nome;
+	
+	@Min(0)
+	private float preco;
+	
+	@Min(0)
+	@Max(1)
+	private float desconto;
 
 	public Produto() {
-		
-	}
-	
-	
-	public Produto(String nome) {
-		super();
-		this.nome = nome;
+
 	}
 
+	public Produto(String nome, float preco, float desconto) {
+		super();
+		this.nome = nome;
+		this.preco = preco;
+		this.desconto = desconto;
+	}
 
 	public int getId() {
 		return id;
@@ -38,6 +50,22 @@ public class Produto {
 
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+
+	public float getPreco() {
+		return preco;
+	}
+
+	public void setPreco(float preco) {
+		this.preco = preco;
+	}
+
+	public float getDesconto() {
+		return desconto;
+	}
+
+	public void setDesconto(float desconto) {
+		this.desconto = desconto;
 	}
 
 }
